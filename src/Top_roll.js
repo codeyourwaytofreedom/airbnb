251248f90643366dd5606726f59752b1f01b316c
codeyourwaytofreedom <can.kolej@gmail.com>
2022-10-06 12:58:38 +0300

    rolling menu almost done. To be made responsive. Keep going. Remember why you started

--- src/Top_roll.js
+++ src/Top_roll.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./Top_roll.css";
 import rolling_options from "./Top_roll_options";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,25 +6,24 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 
 const Top_roll = () => {
 
-    const [index, setIndex] = useState(-30);
 
+    const [index, setIndex] = useState(-3);
+    const x = index+"vw";
+    
     const handle_index_left = () => {
-        if(index===-30)
-            {setIndex(-30)}
+        if(index===-3)
+            {setIndex(-3)}
         else{
-            setIndex(index+500)
+            setIndex(index+26)
         }
-         
-            
+
     }
 
     const handle_index_right = () => {
-        if(index===-1530)
-            {setIndex(-1530)}
-        else{
-            setIndex(index-500)
-        }
-        
+        if(index===-107)
+        {setIndex(index)}
+        else {setIndex(index-26)};
+
     }
 
     return ( 
@@ -33,7 +32,7 @@ const Top_roll = () => {
             <div className="top_roll_content"    
             >   
                 <div className="top_roll_content_test"
-                    style={{ transitionDuration:"0.5s", left:index}}
+                    style={{ transitionDuration:"0.5s", left:x}}
                     >
                     {
                     rolling_options.map((o) => (
@@ -42,7 +41,7 @@ const Top_roll = () => {
                                 <span className="top_roll_content_button_kernel_icon">
                                     <img src={o.icon} alt="x" />
                                 </span>
-                                <span className="top_roll_content_button_kernel_text">{o.text}</span>
+                                <span className="top_roll_content_button_kernel_text">{index}</span>
                             </div>
                         </button>
                     ))
@@ -50,21 +49,22 @@ const Top_roll = () => {
                 </div>
             </div>
                     
-                            <div className="top_roll_content_back" onClick={handle_index_left}
-                                style={{opacity:index===-30 ? "0" : "1"}}
-                            >
-                                <FontAwesomeIcon style={{color:"black"}} size={"l"} icon={faChevronLeft}/>
-                            </div>
-                            <div className="shadow_left" style={{display:index===-30 ? "none" : "block"}}>
-                            </div>
-
+            <div className="top_roll_content_back" onClick={handle_index_left}
+                style={{opacity:index===-3 ? "0" : "1"}}
+                
+            >
+                <FontAwesomeIcon style={{color:"black"}} size={"l"} icon={faChevronLeft}/>
+            </div>
+            <div className="shadow_left" style={{display:index===-3 ? "none" : "block"}}>
+            </div>
 
             <div className="top_roll_content_forth" onClick={handle_index_right}
-                style={{opacity:index===-1530 ? "0" : "1"}}
+                
+                style={{opacity:index===-107 ? "0" : "1"}}
             >
                 <FontAwesomeIcon style={{color:"black"}} size={"l"} icon={faChevronRight}/>
             </div>
-            <div className="shadow_right" style={{display:index===-1530 ? "none" : "block"}}>
+            <div className="shadow_right" style={{display:index===-107 ? "none" : "block"}}>
             </div>
             
             <div className="top_roll_control">
