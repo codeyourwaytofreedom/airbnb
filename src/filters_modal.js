b562777d10eb6330c91c142f3704d5b7e9ba472d
codeyourwaytofreedom <can.kolej@gmail.com>
2022-10-13 19:14:45 +0300

    radio buttons section done default checked options applied. a very fruitful day Remember why you started!

--- src/filters_modal.js
+++ src/filters_modal.js
@@ -8,7 +8,9 @@ import {repetition_array, min, max, av} from "./test";
 const Filters = ({setShow}) => {
 
     const core = useRef();
-    const cb = useRef();
+    const [bedrooms_any, setBedrooms_any] = useState("bedrooms any");
+    const [beds_any, setBeds_any] = useState("beds any");
+    const [bathroom_any, setBathroom_any] = useState("bathrooms any");
     const [pricemin, setPricemin] = useState(min);
     const [pricemax, setPricemax] = useState(max);
     let gap = 10;
@@ -37,7 +39,6 @@ const Filters = ({setShow}) => {
         }
 
     }
-
 
 
     useEffect(()=>{
@@ -148,9 +149,13 @@ const Filters = ({setShow}) => {
                             <div id="bedroom_double">
                                 <div id="bedroom_title"><h3>Bedrooms</h3></div>
                                 <div id="number">
-                                    <label className="radio_button">
+                                    <label className="radio_button" id="any">
                                         
-                                        <input type="radio" name="bedroom" ref={cb}/>
+                                        <input type="radio" name="bedroom" id="any_radiobutton" defaultChecked/>
+                                        <span id="any_span">Any</span>
+                                    </label>
+                                    <label className="radio_button">
+                                        <input type="radio" name="bedroom"/>
                                         <span>1</span>
                                     </label>
                                     <label className="radio_button">
@@ -162,16 +167,128 @@ const Filters = ({setShow}) => {
                                         <input type="radio" name="bedroom"/>
                                         <span>3</span>
                                     </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bedroom"/>
+                                        <span>4</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bedroom"/>
+                                        <span>5</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bedroom"/>
+                                        <span>6</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bedroom"/>
+                                        <span>7</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bedroom"/>
+                                        <span>8+</span>
+                                    </label>
                                     
                                 </div>
                             </div>
                             <div id="beds_double">
-                                <div id="bedroom_title"><h3>Beds</h3></div>
-                                <div id="number">1 2 3 4 5 6 7 8+</div>
+                                <div id="beds_title"><h3>Beds</h3></div>
+                                <div id="number">
+                                <label className="radio_button" id="any">
+                                        <input type="radio" name="beds" id="any_radiobutton" defaultChecked/>
+                                        <span id="any_span">Any</span>
+                                    </label>
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>1</span>
+                                    </label>
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>2</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>3</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>4</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>5</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>6</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>7</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="beds"/>
+                                        <span>8+</span>
+                                    </label>
+                                </div>
                             </div>
                             <div id="bathrooms_double">
-                                <div id="bedroom_title"><h3>Bathrooms</h3></div>
-                                <div id="number">1 2 3 4 5 6 7 8+</div>
+                                <div id="bathrooms_title"><h3>Bathrooms</h3></div>
+                                <div id="number">
+                                <label className="radio_button" id="any">
+                                        
+                                        <input type="radio" name="bathrooms" id="any_radiobutton" defaultChecked/>
+                                        <span id="any_span">Any</span>
+                                    </label>
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>1</span>
+                                    </label>
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>2</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>3</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>4</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>5</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>6</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>7</span>
+                                    </label>
+
+                                    <label className="radio_button">
+                                        <input type="radio" name="bathrooms"/>
+                                        <span>8+</span>
+                                    </label>
+                                </div>
                             </div>
                                 
                             </div>
