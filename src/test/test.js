4bdb99d79722241bda765a3749e5833b6395bcdf
codeyourwaytofreedom <can.kolej@gmail.com>
2022-10-22 12:25:31 +0300

    dummy data improved. fake property objects created! filters can now be applied to the objects. Great start today. Keep working. Remember why you started!

--- src/test/test.js
+++ src/test/test.js
@@ -4,20 +4,83 @@ const create_test_array = () =>{
             // 👇️ get number between min (inclusive) and max (inclusive)
             return Math.floor(Math.random() * (max - min + 1)) + min;
             }
+
+        // const property = (price, type, numberofrooms,numberofbeds, numberofbathrooms) => {
+        //     this.price = price;
+        //     this.type = type;
+        //     this.numberofrooms = numberofrooms;
+        //     this.numberofbeds = numberofbeds;
+        //     this.numberofbathrooms = numberofbathrooms;
+        //     }
+
+            const property = {
+                price: 0,
+                type: String,
+                numberofrooms:0,
+                numberofbeds:0,
+                numberofbathrooms:0
+                };
+              
+
         const price_array = [];
 
         for(var i=0; i<500; i++)
         {
-            price_array.push(randomNumberInRange(9,90)); 
+            const types = ["entire place", "private room", "shared room"]
+            let price = randomNumberInRange(9,90);
+            let type = types[randomNumberInRange(0,2)]
+            let numberofrooms = randomNumberInRange(1,8)
+            let numberofbeds = randomNumberInRange(1,8)
+            let numberofbathrooms = randomNumberInRange(1,8)
+
+            const pro = Object.create(property);
+            pro.price= price;
+            pro.type=type;
+            pro.numberofrooms=numberofrooms;
+            pro.numberofbeds=numberofbeds;
+            pro.numberofbathrooms=numberofbathrooms;
+            console.log(typeof(pro), pro)
+
+            price_array.push(pro); 
         }
         for(var i=0; i<100; i++)
         {
-            price_array.push(randomNumberInRange(90,140)); 
+            const types = ["entire place", "private room", "shared room"]
+            let price = randomNumberInRange(90,140);
+            let type = types[randomNumberInRange(0,2)]
+            let numberofrooms = randomNumberInRange(1,8)
+            let numberofbeds = randomNumberInRange(1,8)
+            let numberofbathrooms = randomNumberInRange(1,8)
+
+            const pro = Object.create(property);
+            pro.price= price;
+            pro.type=type;
+            pro.numberofrooms=numberofrooms;
+            pro.numberofbeds=numberofbeds;
+            pro.numberofbathrooms=numberofbathrooms;
+            console.log(typeof(pro), pro)
+
+            price_array.push(pro); 
         }
         for(var i=0; i<20; i++)
         {
-            price_array.push(randomNumberInRange(200,800)); 
-        }
+            const types = ["entire place", "private room", "shared room"]
+            let price = randomNumberInRange(200,800);
+            let type = types[randomNumberInRange(0,2)]
+            let numberofrooms = randomNumberInRange(1,8)
+            let numberofbeds = randomNumberInRange(1,8)
+            let numberofbathrooms = randomNumberInRange(1,8)
+
+            const pro = Object.create(property);
+            pro.price= price;
+            pro.type=type;
+            pro.numberofrooms=numberofrooms;
+            pro.numberofbeds=numberofbeds;
+            pro.numberofbathrooms=numberofbathrooms;
+            console.log(typeof(pro), pro)
+
+            price_array.push(pro); 
+            }
         console.log(price_array)
 
     return price_array;
@@ -27,17 +90,29 @@ export
 const calc_price_repetition = (array) => {
 
         let repetition_array = [];
+        const min_price = array.reduce((previous, current) => {
+            return current.price < previous.price ? current : previous;
+          });
+        
+        const max_price = array.reduce((previous, current) => {
+        return current.price > previous.price ? current : previous;
+        });
+
         for(var i=0; i<50; i++)
         {
             var range_start = 
-                    Math.min(...array)+(Math.max(...array)-Math.min(...array))/50*i;
+                    // Math.min(...array)+(Math.max(...array)-Math.min(...array))/50*i;
+                    min_price+ (max_price-min_price)/50*i;
 
-            var range_end = (Math.max(...array)-Math.min(...array))/50*(i+1);
+            var range_end = 
+                            // (Math.max(...array)-Math.min(...array))/50*(i+1);
+                            min_price+(max_price-min_price)/50*(i+1);
+
             
             let how_many = 0;
 
             array.forEach(element => {
-                if(element>range_start && element<=range_end )
+                if(element.price>range_start && element.price<=range_end )
                 {how_many++}
             });
             repetition_array.push(how_many)                       
@@ -52,7 +127,7 @@ const test = create_test_array()
 
 let average = 0;
 test.forEach((price)=>{
-        average=average+price
+        average=average+price.price
 })
 
 export let numOfproperties = test.length;
@@ -61,6 +136,10 @@ export const av = Math.floor(average/test.length);
 
 export const repetition_array = calc_price_repetition(test);
 
-export const min = Math.min(...test);
-export const max = Math.max(...test);
+export const min = test.reduce((previous, current) => {
+    return current.price < previous.price ? current : previous;
+  }).price;
+export const max = test.reduce((previous, current) => {
+    return current.price > previous.price ? current : previous;
+    }).price;
 
