899ee6abbb28e2e5367cf07906689ce3de28b553
codeyourwaytofreedom <can.kolej@gmail.com>
2022-10-31 23:40:02 +0300

    Bottom banner constructed. modals to be built for items in banner. aspect ration worked.
    Remember why you started!

--- src/bottom-banner/bottom_banner.js
+++ src/bottom-banner/bottom_banner.js
@@ -1,20 +1,41 @@
 import "./bottom_banner.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faChevronUp, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const Bottom_banner = () => {
     return ( 
         <div className="bottom_banner">
             <div className="bottom_banner_left">
-                <div>© 2022 Airbnb, Inc.</div>
-                <div>x</div>
-                <div>x</div>
-                <div>x</div>
-                <div>x</div>
+                <div className="bottom_banner_left_cell">© 2022 Airbnb, Inc.</div>
+                <span> . </span>
+                <div className="bottom_banner_left_cell">Privacy</div>
+                <span> . </span>
+                <div className="bottom_banner_left_cell">Terms</div>
+                <span> . </span>
+                <div className="bottom_banner_left_cell">Sitemap</div>
+                <span> . </span>
+                <div className="bottom_banner_left_cell">UK Modern Slavery Act</div>
+                <span> . </span>
+                <div className="bottom_banner_left_cell">Company details</div>
+                <span> . </span>
+                <div className="bottom_banner_left_cell">Destinations</div>
             </div>
 
             <div className="bottom_banner_right">
-                <div>x</div>
-                <div>x</div>
-                <div>x</div>
+                <div className="bottom_banner_left_cell--globe">
+                    <FontAwesomeIcon style={{color:"black"}} icon={faGlobe}/>
+                    <div>English (GB)</div>
+                    
+                </div>
+                <div className="bottom_banner_left_cell--currency">
+                    <div>£ </div>  
+                    <div>GBP</div>
+                    
+                </div>
+                <div className="bottom_banner_left_cell--support">
+                    <div>Support & resources</div>
+                    <div><FontAwesomeIcon style={{color:"black"}} icon={faChevronUp}/></div>
+                </div>
             </div>
         </div>
      );
