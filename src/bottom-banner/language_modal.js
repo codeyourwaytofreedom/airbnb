9b57182dc0fc4ff1c69539d1e1f27a07d73f2730
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-01 17:10:03 +0300

    language modal part one done!

--- src/bottom-banner/language_modal.js
+++ src/bottom-banner/language_modal.js
@@ -2,7 +2,23 @@ import "../modals/modal.css";
 
 export const Lan = 
                 <div className="modal_panel_language-shell">
-                    Hello World
+                    <div className="translation">
+                        <div className="translation_panel">
+                            <div className="translation_panel_double">
+                               <div className="translation_panel_double_action">Translation</div>
+                                <div className="translation_panel_double_explain">
+                                Automatically translate descriptions and reviews to English.
+                                </div> 
+                            </div>
+                            <div className="toggle">
+                                    <label className="switch">
+                                        <input type="checkbox"/>
+                                        <span className="slider round"></span>
+                                    </label>
+                            </div>
+                            
+                        </div>
+                    </div>
                     HEllo
                 </div>
 
