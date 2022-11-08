aee4ec823d7aa6674a7878ad1c48528b3c7c5646
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-08 14:56:49 +0300

    JSX + css added for search modal.

--- src/head/head.js
+++ src/head/head.js
@@ -97,21 +97,43 @@ const Head = () => {
                 </div>
                 <div className="hd_search_shll_extension-center">
                     <div className="hd_search_shll_click-extension">
-                    <div className="stays_tab">
-                        <div>Where</div>
-                        <div>Search destinations</div>
+                    <div className="stays_tab" tabIndex={1}>
+                        <div className="stays_tab_shell">
+                            <div className="top">Where</div>
+                            <div className="bottom">Search destinations</div>
+                        </div>
+                        
                     </div>
-                    <div className="stays_tab">
-                        <div>Check-in</div>
-                        <div>Add dates</div>
+                    <div className="buffer"></div>
+                    <div className="stays_tab" tabIndex={2}>
+                        <div className="stays_tab_shell">
+                            <div className="top">Check-in</div>
+                            <div className="bottom">Add dates</div>
+                        </div>
+                        
                     </div>
-                    <div className="stays_tab">
-                        <div>Check-out</div>
-                        <div>Add dates</div>
+                    <div className="buffer"></div>
+                    <div className="stays_tab" tabIndex={3}>
+                        <div className="stays_tab_shell">
+                            <div className="top">Check-out</div>
+                            <div className="bottom">Add dates</div>
+                        </div>
+                        
                     </div>
-                    <div className="stays_tab">
-                        <div>Who</div>
-                        <div>Add guests</div>
+                    <div className="buffer"></div>
+                    <div className="stays_tab" id="who" tabIndex={4}>
+                        <div className="stays_tab_shell">
+                            <div className="top">Who</div>
+                            <div className="bottom">Add guests</div>
+                        </div>
+                        <div className="stays_tab_search">
+                             <button id="search_button">
+                                <FontAwesomeIcon icon={faSearch} color={"white"} />
+                                <span>Search</span>
+                            </button>
+                        </div>
+                       
+                        
                     </div>
                 </div>
                 </div>
