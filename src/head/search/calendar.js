7da673b10f73ed2fccac1e1c3dd3b7a7df97367b
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-12 21:03:05 +0300

    keyboard shortcuts removed from calendar. Calendar to be further prettified.
    Keep going.

--- src/head/search/calendar.js
+++ src/head/search/calendar.js
@@ -4,6 +4,8 @@ import 'react-dates/initialize';
 import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 import moment from 'moment';
+import "./calendar.css";
+
 
 
 
@@ -17,9 +19,9 @@ const Calendar_comp = () => {
         <DayPickerRangeController
             startDate={null} // momentPropTypes.momentObj or null,
             endDate={null} // momentPropTypes.momentObj or null,
-            onDatesChange={null} // PropTypes.func.isRequired,
+            onDatesChange={(null)} // PropTypes.func.isRequired,
             focusedInput={null} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-            onFocusChange={null} // PropTypes.func.isRequired,
+            onFocusChange={(null)} // PropTypes.func.isRequired,
             // initialVisibleMonth={() => moment().add(2, "M")} // PropTypes.func or null,
             numberOfMonths={2} 
             noBorder={true}
