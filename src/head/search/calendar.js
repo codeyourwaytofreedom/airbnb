8ddf4b720cd61d5e32ef79d28ba75a4e7aeb8b86
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-13 11:35:55 +0300

    onDatesChange and onFocusChange fucntions ready. Calendar date select works. default styles being replaced. selected day css done. selected range css tested and done.
    good milestone for calendar.
    
    Keep going.
    
    Remember why you started.

--- src/head/search/calendar.js
+++ src/head/search/calendar.js
@@ -13,16 +13,28 @@ import Calendar from 'react-calendar';
 
 const Calendar_comp = () => {
 
+    const [dates, setDates] = useState({ startDate: null, endDate: null });
+  
+    const defaultFocusedInput = "startDate";
+    const [focusedInput, setFocusedInput] = useState(defaultFocusedInput);
+    const handleDatesChange = (dates) => {
+      setDates(dates);
+    };
+  
+    const onFocusChange = (focusedInput) => {
+      console.log(onFocusChange);
+      setFocusedInput(focusedInput);
+    };
+
 
 
     return ( 
         <DayPickerRangeController
-            startDate={null} // momentPropTypes.momentObj or null,
-            endDate={null} // momentPropTypes.momentObj or null,
-            onDatesChange={(null)} // PropTypes.func.isRequired,
-            focusedInput={null} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-            onFocusChange={(null)} // PropTypes.func.isRequired,
-            // initialVisibleMonth={() => moment().add(2, "M")} // PropTypes.func or null,
+            startDate={dates.startDate} 
+            endDate={dates.endDate} 
+            onDatesChange={handleDatesChange} 
+            focusedInput={focusedInput || defaultFocusedInput} 
+            onFocusChange={onFocusChange}
             numberOfMonths={2} 
             noBorder={true}
         />
